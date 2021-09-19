2066b258044a962e17f1788b5857f8b5566cfacd
davideruisi <d.ruisi94@gmail.com>
2021-09-19 13:12:02 +0200

    Add Katana and Tempura dependencies

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '13.0'
+
+target 'HSMaster' do
+  use_frameworks!
+
+  pod 'Katana', '~> 6.0'
+  pod 'Tempura', '~> 9.0'
+end
--- setup.sh
+++ setup.sh
@@ -5,9 +5,12 @@ if ! command -v xcodegen &> /dev/null
 then
     echo "XcodeGen could not be found and will be installed."
     brew install xcodegen
-    exit
 fi
 
 # Generate xcodeproj file.
 echo "Generating xcodeproj file."
 xcodegen generate
+
+# Install pods.
+echo "Installing pods."
+pod install
