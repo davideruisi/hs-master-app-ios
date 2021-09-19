f0e2ca8bd54fc0026b139452b96bb244ff802a6b
davideruisi <d.ruisi94@gmail.com>
2021-09-20 00:46:11 +0200

    Add swiftlint

--- HSMaster/AppDelegate.swift
+++ HSMaster/AppDelegate.swift
@@ -10,16 +10,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
-  
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+  func application(
+    _ application: UIApplication,
+    // swiftlint:disable:next discouraged_optional_collection
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
     let window = UIWindow()
     let vc = ViewController()
     window.rootViewController = vc
     self.window = window
-    
+
     window.makeKeyAndVisible()
-    
+
     return true
   }
 }
-
--- Podfile
+++ Podfile
@@ -4,5 +4,6 @@ target 'HSMaster' do
   use_frameworks!
 
   pod 'Katana', '~> 6.0'
+  pod 'SwiftLint', '~> 0.44'
   pod 'Tempura', '~> 9.0'
 end
