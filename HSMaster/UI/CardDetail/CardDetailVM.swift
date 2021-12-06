00fecd717a4d8b486711f44a7f2579836224ac23
davideruisi <d.ruisi94@gmail.com>
2021-12-06 20:21:52 +0100

    CardDetail: Add CardDetailView with VM

---
+++ HSMaster/UI/CardDetail/CardDetailVM.swift
@@ -0,0 +1,130 @@
+//
+//  CardDetailVM.swift
+//  HSMaster
+//
+//  Created by Davide Ruisi on 30/11/21.
+//
+
+import Foundation
+import Tempura
+
+// MARK: - ViewModel
+
+/// The ViewModel of the CardDetailView.
+struct CardDetailVM: ViewModelWithLocalState {
+
+  // MARK: Stored Properties
+
+  /// The game metadata.
+  private let metadata: Models.Metadata?
+
+  /// The card shown in the detail view.
+  let card: Models.Card
+
+  // MARK: Init
+
+  init?(state: AppState?, localState: CardDetailLS) {
+    metadata = state?.metadata
+    card = localState.card
+  }
+}
+
+// MARK: - ViewModel Helpers
+
+extension CardDetailVM {
+
+  // MARK: Set Detail
+
+  var cardSet: Models.Metadata.Set? {
+    metadata?.getSet(with: card.setId)
+  }
+
+  var isSetDetailHidden: Bool {
+    cardSet == nil
+  }
+
+  var setDetailBody: NSAttributedString? {
+    NSAttributedString(string: cardSet?.name ?? "")
+  }
+
+  var setDetailVM: DetailVM {
+    DetailVM(title: Localization.CardDetail.set, body: setDetailBody, isHidden: isSetDetailHidden)
+  }
+
+  // MARK: Class Detail
+
+  var cardClasses: [Models.Metadata.Class] {
+    card.classIds.compactMap { metadata?.getClass(with: $0) }
+  }
+
+  var isClassDetailHidden: Bool {
+    cardClasses.isEmpty
+  }
+
+  var classDetailBody: NSAttributedString? {
+    guard let firstClass = cardClasses.first else {
+      return nil
+    }
+
+    let string = cardClasses.dropFirst().reduce(firstClass.name) { "\($0), \($1.name)" }
+
+    return NSAttributedString(string: string)
+  }
+
+  var classDetailVM: DetailVM {
+    DetailVM(title: Localization.CardDetail.class, body: classDetailBody, isHidden: isClassDetailHidden)
+  }
+
+  // MARK: Keyword Detail
+
+  var keywords: [Models.Metadata.Keyword] {
+    card.keywordIds.compactMap { metadata?.getKeyword(with: $0) }
+  }
+
+  var isKeywordDetailHidden: Bool {
+    keywords.isEmpty
+  }
+
+  var keywordDetailBody: NSAttributedString? {
+    guard let firstKeyword = keywords.first else {
+      return nil
+    }
+
+    let boldFont = UIFont.systemFont(ofSize: 18, weight: .bold)
+
+    let attributedString = NSMutableAttributedString(
+      string: firstKeyword.name,
+      attributes: [.font: boldFont]
+    )
+    attributedString.append(NSAttributedString(string: ": \"\(firstKeyword.description)\""))
+
+    _ = keywords.dropFirst().reduce(into: attributedString) {
+      $0.append(NSAttributedString(string: "\n\($1.name)", attributes: [.font: boldFont]))
+      $0.append(NSAttributedString(string: ": \"\($1.description)\""))
+    }
+
+    return attributedString
+  }
+
+  var keywordDetailVM: DetailVM {
+    DetailVM(title: Localization.CardDetail.keywords, body: keywordDetailBody, isHidden: isKeywordDetailHidden)
+  }
+
+  // MARK: Artist Detail
+
+  var artistDetailVM: DetailVM {
+    guard let artistName = card.artistName else {
+      return DetailVM(title: nil, body: nil, isHidden: true)
+    }
+
+    return DetailVM(title: Localization.CardDetail.artist, body: NSAttributedString(string: artistName), isHidden: false)
+  }
+}
+
+// MARK: - LocalState
+
+/// The LocalState of the CardDetailView.
+struct CardDetailLS: LocalState {
+  /// The card shown in the detail view.
+  let card: Models.Card
+}
---
+++ HSMaster/UI/CardDetail/CardDetailView.swift
@@ -0,0 +1,150 @@
+//
+//  CardDetailView.swift
+//  HSMaster
+//
+//  Created by Davide Ruisi on 30/11/21.
+//
+
+import PinLayout
+import Tempura
+import UIKit
+
+/// The view showing the detail of a card.
+final class CardDetailView: UIView, ViewControllerModellableView {
+
+  // MARK: - Constants
+
+  static let cardImageRatio: CGFloat = 375.0 / 518.0
+
+  // MARK: - UI Elements
+
+  let scrollView = UIScrollView()
+  let containerView = UIView()
+  let cardImageView = UIImageView()
+  let flavorTextLabel = UILabel()
+  let setDetailView = DetailView()
+  let classDetailView = DetailView()
+  let keywordDetailView = DetailView()
+  let artistDetailView = DetailView()
+
+  // MARK: - Interactions
+
+  var didTapCloseButton: Interaction?
+
+  // MARK: - SSUL
+
+  func setup() {
+    addSubview(scrollView)
+    scrollView.addSubview(containerView)
+    containerView.addSubview(cardImageView)
+    containerView.addSubview(flavorTextLabel)
+    containerView.addSubview(setDetailView)
+    containerView.addSubview(classDetailView)
+    containerView.addSubview(keywordDetailView)
+    containerView.addSubview(artistDetailView)
+
+    navigationItem?.rightBarButtonItem = UIBarButtonItem(
+      barButtonSystemItem: .close,
+      target: self,
+      action: #selector(tappedCloseButton)
+    )
+  }
+
+  func style() {
+    Self.Style.view(self)
+  }
+
+  func update(oldModel: CardDetailVM?) {
+    Self.Style.cardImageView(cardImageView, with: model?.card.imageURL)
+    Self.Style.navigationItem(navigationItem, with: model?.card.name)
+
+    Self.Style.flavorTextLabel(flavorTextLabel, with: model?.card.flavorText)
+
+    setDetailView.model = model?.setDetailVM
+    classDetailView.model = model?.classDetailVM
+    keywordDetailView.model = model?.keywordDetailVM
+    artistDetailView.model = model?.artistDetailVM
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+
+    scrollView.pin
+      .all()
+
+    containerView.pin
+      .top()
+      .horizontally()
+
+    cardImageView.pin
+      .horizontally(SharedStyle.Spacing.large)
+      .aspectRatio(Self.cardImageRatio)
+
+    flavorTextLabel.pin
+      .below(of: cardImageView)
+      .marginTop(SharedStyle.Spacing.medium)
+      .horizontally(SharedStyle.Spacing.medium)
+      .sizeToFit(.width)
+
+    setDetailView.pin
+      .below(of: flavorTextLabel)
+      .marginTop(SharedStyle.Spacing.large)
+      .horizontally(SharedStyle.Spacing.medium)
+      .sizeToFit(.width)
+
+    classDetailView.pin
+      .below(of: setDetailView)
+      .horizontally(SharedStyle.Spacing.medium)
+      .sizeToFit(.width)
+
+    keywordDetailView.pin
+      .below(of: classDetailView)
+      .horizontally(SharedStyle.Spacing.medium)
+      .sizeToFit(.width)
+
+    artistDetailView.pin
+      .below(of: keywordDetailView)
+      .horizontally(SharedStyle.Spacing.medium)
+      .sizeToFit(.width)
+
+    containerView.pin
+      .wrapContent(.vertically, padding: SharedStyle.Spacing.small)
+
+    scrollView.contentSize = CGSize(width: scrollView.frame.width, height: containerView.frame.height)
+  }
+}
+
+// MARK: - Styling Functions
+
+extension CardDetailView {
+  enum Style {
+    static func view(_ view: CardDetailView) {
+      view.backgroundColor = Palette.backgroundPrimary.color
+    }
+
+    static func navigationItem(_ navigationItem: UINavigationItem?, with title: String?) {
+      navigationItem?.title = title
+    }
+
+    static func cardImageView(_ imageView: UIImageView, with url: URL?) {
+      imageView.contentMode = .scaleAspectFit
+      imageView.kf.setImage(with: url)
+    }
+
+    static func flavorTextLabel(_ label: UILabel, with text: String?) {
+      label.text = text
+      label.font = .italicSystemFont(ofSize: 18)
+      label.numberOfLines = 0
+      label.textAlignment = .center
+    }
+  }
+}
+
+// MARK: - Helpers
+
+private extension CardDetailView {
+  @objc
+  func tappedCloseButton() {
+    didTapCloseButton?()
+  }
+}
